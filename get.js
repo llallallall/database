173c5241a593f5069f544e52c3972f9b9f7ed670
Hwang MyeongHwan <llallallall@kakao.com>
2023-01-13 15:45:35 +0900

    applications 추가

---
+++ get.js
@@ -0,0 +1,47 @@
+module.exports.setup = function (app, db) {
+        app.get('/', (req, res, next) => {
+                res.json({ 'rsp' : 'ok'})
+        })
+
+        app.get('/db/about-me', (req, res, next) => {
+                let result = {
+                        'rsp' : 'fail'
+                }
+        
+                db.get('SELECT * FROM tbl_about_myself', (err, row) => {
+                        if(!err) {
+                                result.data = row
+                                db.all('SELECT * FROM tbl_my_resume ORDER BY date desc', (err2, rows) => {
+                                        if(!err2) {
+                                                result.rsp = 'ok',
+                                                result.data.resume = rows
+                                                res.json(result)
+                                        } else {
+                                                res.json(result)
+                                        }
+                                })
+                                
+                        } else {
+                                res.json(result)
+                                console.log('Error Query')
+                        }
+                })
+        }) 
+
+        app.get('/db/applications', (req, res, next) => {
+                let result = {
+                        'rsp' : 'fail',
+                }
+                db.all('SELECT * FROM tbl_applications ORDER BY date desc', (err, rows) => {
+                        if (!err) {
+                                result.rsp = 'ok'
+                                result.data = rows
+                                res.json(result)
+                                console.log(`${rows[0].name}`)
+                        } else {
+                                res.json(result)
+                                console.log('Error Query')
+                        }
+                })
+        })
+}
---
+++ initial.js
@@ -0,0 +1,90 @@
+const TYPE = require('./type.js')
+
+function fn_about_me(db) {
+        db.run('CREATE TABLE IF NOT EXISTS tbl_about_myself (name TEXT, email TEXT, UNIQUE(name, email))', 
+                (err) => {
+                        if (!err) {
+                                db.run(
+                                        "INSERT OR IGNORE INTO tbl_about_myself (name, email) VALUES ('laon','llallallall@kakako.com')"
+                                )
+                        }
+                })
+}
+
+function fn_resume(db) {
+        db.run('CREATE TABLE IF NOT EXISTS tbl_my_resume (date DATE, title TEXT, content TEXT, url TEXT, UNIQUE(date, title))',
+                (err) => {
+                        if(!err) {
+                                const resume = [
+                                        {
+                                                date : '1981-12-18',
+                                                title: '탄생',
+                                                content : '널리 이롭게 하리라',
+                                                url : null,
+                                        },
+                                        {
+                                                date : '2000-03-01',
+                                                title : '대학 입학',
+                                                content : '고려대학교 서어서문',
+                                                url : null,
+                                        },
+                                        {
+                                                date : '2009-11-02',
+                                                title : '입사',
+                                                content : '아씨..',
+                                                url : 'https://www.at.or.kr',
+                                        }
+                                ]
+
+                                resume.forEach((item) => {
+                                        const query = `INSERT OR IGNORE INTO tbl_my_resume (date, title, content, URL) VALUES ('${item.date}','${item.title}','${item.content}','${item.URL}')`
+                                        db.run(query)
+                                })
+                        }
+                }
+        )
+}
+
+function fn_applications(db) {
+        db.run('CREATE TABLE IF NOT EXISTS tbl_applications (id INT, name TEXT, content TEXT, date DATE, platform TEXT, url TEXT, image TEXT, UNIQUE(name, date))',
+                (err) => {
+                        if (!err) {
+                                const applications = [
+                                        {
+                                                id:1,
+                                                name :'힘을 내라',
+                                                content : '응원 어플',
+                                                date : '2021-08-01',
+                                                platform : 'Web',
+                                                url : 'http://triego.kr',
+                                                image : 'https://cdn.crowdpic.net/detail-thumb/thumb_d_DB5D105678D987231D0C2757B1714FD4.jpg'
+                                        },
+                                        {
+                                                id:2,
+                                                name :'웃어라',
+                                                content : '힐링 어플',
+                                                date : '2022-08-01',
+                                                platform : 'Web',
+                                                url : 'http://muso.kr',
+                                                image : 'https://image.yes24.com/momo/TopCate02/MidCate10/199084.jpg'
+                                        }
+                                ]
+
+                                applications.forEach((item) => {
+                                        const query = `INSERT OR IGNORE INTO tbl_applications (id, name, content, date, platform, url, image) VALUES ('${item.id}','${item.name}','${item.content}','${item.date}','${item.platform}','${item.url}','${item.image}')`
+                                        db.run(query)
+                                })
+                        }
+                }
+        )
+}
+
+module.exports.run = function (db, type) {
+        if (type == TYPE.about_me) {
+                fn_about_me(db)
+        } else if ( type == TYPE.resume) {
+                fn_resume(db)
+        } else if ( type == TYPE.applications) {
+                fn_applications(db)
+        }
+}
